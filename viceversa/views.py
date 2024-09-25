0e3f5e3e7a75ed86ef5efc178ebced2556ef4a80
Roman Kriukov <romankriukov@MacBook-Pro-Roman.local>
2024-09-25 11:52:22 +0300

    Add counter words

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from collections import Counter
 
 
 def home(request):
@@ -8,6 +9,8 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    counter_words = len(Counter(user_text.split()))
     reversed_text = user_text[::-1]
     return render(request, 'reverse.html', {'usertext': user_text,
-                                            'reversedtext': reversed_text})
+                                            'reversedtext': reversed_text,
+                                            'counterwords': counter_words})
