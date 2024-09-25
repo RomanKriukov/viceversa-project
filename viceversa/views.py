599b0d34a7acf1349e1726489700a96124f696f2
Roman Kriukov <romankriukov@MacBook-Pro-Roman.local>
2024-09-25 11:39:23 +0300

    Add reverse feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'greeting': 'Hello from views!'})
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext': user_text,
+                                            'reversedtext': reversed_text})
